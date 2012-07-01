92d84ba37e7dc38fb1e765690815e159c10a4ee4
ashutosh <ashutoshraina1989@gmail.com>
2012-07-01 19:42:39 +0530

    WithConsoleApp

---
+++ FreebaseConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreebaseConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var thepolice = new Question { type = "/music/artist" };
+            var response = ConnectToFreebase.Connect(thepolice);
+            Console.ReadLine();
+        }
+    }
+}
